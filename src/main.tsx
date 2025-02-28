cda63d26608bbd0f1adb728c67636fc3194fb6da
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2025-02-27 19:20:02 -0600

    Added normalize.css.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
 import './assets/styles/global/tailwind.css'
+import './assets/styles/global/normalize.css'
+
+
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
